29f2a289cf29c1a03393d13194808226dc14a083
Lucas Siqueira <lucasbferreira14@gmail.com>
2023-06-16 18:09:47 -0300

    Img setattribute

--- scritp.js
+++ scritp.js
@@ -6,9 +6,9 @@ function toogleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar.png")
-  } else {
     img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
 
